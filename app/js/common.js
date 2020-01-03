22812fd82ac2987d01d5b03cfbfbdb86b60bb0da
KepCoil <perekonwork@yandex.ru>
2020-01-03 18:55:11 +0700

    section services ready

--- app/js/common.js
+++ app/js/common.js
@@ -23,4 +23,60 @@ $(function() {
 		$('#js-menu-btn').removeClass('is-active');
 	});
 
+
+	/* OWL CAROUSEL */
+	$('.js-carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function() {
+			carouselServiceHeight();
+		}, 100);
+	});
+
+	$('.js-carousel-services').owlCarousel({
+		nav: true,
+		smartSpeed: 700,
+		dots: false,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			}
+		}
+	});
+
+
+	function carouselServiceHeight() {
+		$('.carousel-services__item').each(function() {
+			var 
+				ths 		 = $(this),
+				thsHeight = ths.find('.carousel-services__item-content').outerHeight();
+
+			ths.find('.carousel-services__item-img').css('min-height', thsHeight);
+
+		});
+	} carouselServiceHeight();
+
+	$('.js-carousel-services__item-title').each(function() {
+		var ths  = $(this);
+		ths.html( ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+
+	/* END OWL CAROUSEL */
+
+	function onResize() {
+		$('.carousel-services__item-composition').equalHeights();
+	} onResize();
+	
+
+	/* При ресайзе страницы */
+	window.onresize = function () {
+		onResize();
+	};
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,9 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/mmenu/js/jquery.mmenu.all.min.js',
-		'app/js/common.js', // Always at the end
+		'app/libs/owl.carousel/owl.carousel.min.js',
+		'app/libs/equalHeights/equalheights.js',
+		'app/js/common.js' // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
